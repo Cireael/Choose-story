93fb5ade67ec8b73137c5f686a0d518416a272b9
Cireael <oren.si@yandex.ru>
2022-09-15 00:11:27 +0300

    first version

---
+++ Choose story/Choose story/Model/Story.swift
@@ -0,0 +1,16 @@
+//
+//  Story.swift
+//  Choose story
+//
+//  Created by Julia on 07.08.2022.
+//
+
+import Foundation
+
+struct Story {
+    let title: String
+    let choice1: String
+    let choice1Destination: Int
+    let choice2: String
+    let choice2Destination: Int
+}
